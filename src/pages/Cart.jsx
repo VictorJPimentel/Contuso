044d3576736900872f3043536bd31333a04342d0
Victor Pimentel <victorjosuepimentel21@gmail.com>
2022-08-05 19:47:25 -0400

    Cart styled

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -8,7 +8,7 @@ import createCheckoutSession from "../helpers/createCheckoutSession";
 
 function Cart() {
   const { cart } = useCartContext();
-  // console.log(cart);
+  console.log(cart);
 
   const [isLoginModal, setIsLoginModal] = useState(false);
 
@@ -34,23 +34,79 @@ function Cart() {
   }
 
   return (
-    <div className="w-screen relative h-screen flex flex-col bg-blue-300 ">
+    <div className="container mx-auto mt-10">
+      <div className="flex shadow-md my-10">
+        <div className="w-full bg-white px-10 py-10">
+          <div className="flex justify-between border-b pb-8">
+            <h1 className="font-semibold text-2xl">Shopping Cart</h1>
+            <h2 className="font-semibold text-2xl">{cart.length} Items</h2>
+          </div>
+          <div className="flex mt-10 mb-5">
+            <h3 className="font-semibold text-gray-600 text-xs uppercase w-full">
+              Product Details
+            </h3>
+            <h3 className="font-semibold text-center text-gray-600 text-xs uppercase w-1/5 text-center">
+              Total
+            </h3>
+          </div>
+          {cart?.map((product) => (
+            <div className="flex items-center hover:bg-gray-100 -mx-8 px-6 py-5">
+              <div className="flex w-full">
+                {" "}
+                {/* product */}
+                <div className="w-20">
+                  <img className="h-24" src={product.images[0]} alt="" />
+                </div>
+                <div className="flex flex-col justify-between ml-4 flex-grow">
+                  <span className="font-bold text-sm">{product.name}</span>
+                </div>
+              </div>
+
+              <span className="text-center w-1/5 font-semibold text-sm">
+                {product.price.unit_amount / 100}{" "}
+                {product.price.currency.toUpperCase()}
+              </span>
+            </div>
+          ))}
+          <button
+            onClick={isAuth}
+            className="bg-primary font-semibold hover:bg-primary-600 py-3 text-sm text-white uppercase w-full"
+          >
+            Checkout
+          </button>{" "}
+        </div>
+      </div>
+      <Link to="/" className="flex font-semibold text-bg-primary text-sm mt-10">
+        <svg
+          className="fill-current mr-2 text-bg-primary w-4"
+          viewBox="0 0 448 512"
+        >
+          <path d="M134.059 296H436c6.627 0 12-5.373 12-12v-56c0-6.627-5.373-12-12-12H134.059v-46.059c0-21.382-25.851-32.09-40.971-16.971L7.029 239.029c-9.373 9.373-9.373 24.569 0 33.941l86.059 86.059c15.119 15.119 40.971 4.411 40.971-16.971V296z" />
+        </svg>
+        Continue Shopping
+      </Link>
       <div
         id="buy-modal"
-        className={`absolute top-0 left-0  bg-green-400/10 w-screen h-screen z-30 backdrop-blur-md flex flex-col justify-center items-center ${
+        className={`absolute top-0 left-0 w-screen h-screen z-30 backdrop-blur-md flex flex-col justify-center items-center ${
           isLoginModal ? "block" : "hidden"
         }`}
       >
-        <div className="bg-white w-1/5 h-1/3 flex flex-col items-center justify-evenly">
-          <button onClick={login}>Sign in with Google</button>
-        </div>
+        <button
+          onClick={login}
+          aria-label="Continue with google"
+          role="button"
+          class="focus:outline-none focus:ring-2 focus:ring-offset-1 focus:ring-gray-700 py-3.5 px-4 border rounded-lg shadow flex items-center mt-10"
+        >
+          <img
+            src="https://tuk-cdn.s3.amazonaws.com/can-uploader/sign_in-svg2.svg"
+            alt="google"
+          />
+          <p class="text-base font-medium ml-4 text-gray-700">
+            Continue with Google
+          </p>
+        </button>
       </div>
-      Your cart:
-      {cart?.map((product) => (
-        <p>{product.name}</p>
-      ))}
-      <button onClick={isAuth}>Buy</button>
-      <Link to="/">Back to home</Link>
+
       <div />
     </div>
   );
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,9 @@ import getActiveProducts from "../helpers/getActiveProducts";
 import ItemCard from "../components/itemCard";
 import { signOut } from "firebase/auth";
 import { auth } from "../firebase/credentials";
+import { useUserContext } from "../contexts/userContext";
 function Home() {
+  const { user, setUser } = useUserContext();
   const [products, setProducts] = useState(null);
 
   useEffect(() => {
@@ -22,144 +24,145 @@ function Home() {
 
   return (
     <>
-      {/* <button onClick={logout}>Logout</button> */}
-    <nav
-      class="flex lg:sticky top-0   bg-header opacity-90"
-    >
-      <div
-        class="container flex flex-wrap px-4 py-2 mx-auto lg:mx-0 lg:space-x-4"
-      >
-        <a
-          href=""
-          class="inline-flex p-2 text-stone-300 text-xl font-bold uppercase tracking-wider font-title"
-          >CONTUSO</a
-        >
-        <div
-          class="w-full lg:inline-flex lg:w-auto mt-2 lg:mt-0"
-        >
-          <ul
-            class="w-full lg:w-auto flex flex-col lg:flex-row space-y-2 lg:space-x-2 lg:space-y-0"
+      <nav class="flex lg:sticky top-0   bg-header opacity-90">
+        <div class="container flex flex-wrap px-4 py-2 mx-auto lg:mx-0 lg:space-x-4">
+          <a
+            href=""
+            class="inline-flex p-2 text-stone-300 text-xl font-bold uppercase tracking-wider font-title"
           >
-            <li>
-              <a
-                href="#"
-                class="flex px-4 py-2 font-content text-stone-300 hover:bg-stone-700 rounded-md"
-                >Home</a
-              >
-            </li>
-            <li class="relative">
-              <a
-                href="#products"
-                class="flex w-full outline-none focus:outline-none px-4 py-2 font-content text-stone-300 hover:bg-stone-700 rounded-md"
-
-              >
-                Products
-              </a>
-            </li>
-            {/* <li>
-              <a
-                href="./contactUs.html"
-                class="flex px-4 py-2 font-content text-stone-300 hover:bg-stone-700 rounded-md"
-                >Contact us
+            CONTUSO
+          </a>
+          <div class="w-full lg:inline-flex lg:w-auto mt-2 lg:mt-0">
+            <ul class="w-full lg:w-auto flex flex-col lg:flex-row space-y-2 lg:space-x-2 lg:space-y-0">
+              <li>
+                <a
+                  href="#"
+                  class="flex px-4 py-2 font-content text-stone-300 hover:bg-stone-700 rounded-md"
+                >
+                  Home
                 </a>
-            </li> */}
-          </ul>
+              </li>
+              <li class="relative">
+                <a
+                  href="#products"
+                  class="flex w-full outline-none focus:outline-none px-4 py-2 font-content text-stone-300 hover:bg-stone-700 rounded-md"
+                >
+                  Products
+                </a>
+              </li>
+              {user ? (
+                <li>
+                  <button
+                    class="flex w-full outline-none focus:outline-none px-4 py-2 font-content text-stone-300 hover:bg-stone-700 rounded-md"
+                    onClick={logout}
+                  >
+                    Logout
+                  </button>
+                </li>
+              ) : null}
+            </ul>
+          </div>
+        </div>
+      </nav>
+      <section class="flex items-center w-full justify-center h-screen mb-0 bg-fixed bg-center bg-cover bg-main-page">
+        <div class="p-5 lg:text-main-title md:text-7xl text-6xl text-stone-300 rounded-md bg-primary capitalize font-title opacity-90">
+          CONTUSO
+        </div>
+      </section>
+      <div class="w-auto bg-primary p-10">
+        <div class="max-w-lg m-auto">
+          <h1 class="text-5xl text-center ml-0 font-title uppercase mb-4 text-stone-300">
+            The Most Reliable Car parts in the industry
+          </h1>
+          <p class="mb-4 m-auto text-lg text-left font-content text-secondary">
+            From all over the globe our products have impacted thousands of
+            lifes by imporing the way their cars function. Our teams focus on
+            maximizing the quality of our car parts so that you never have to
+            worry about a car malfunction
+          </p>
+
+          <p class="mb-4 m-auto text-lg text-left font-content text-secondary">
+            We provide personal vehicle owners and enthusiasts with the{" "}
+            <b>vehicle related products</b> and <b>knowledge</b> that fulfill
+            their wants and needs at the right price.
+          </p>
         </div>
       </div>
-    </nav>
-    <section
-      class="flex items-center w-full justify-center h-screen mb-0 bg-fixed bg-center bg-cover bg-main-page"
-    >
       <div
-        class="p-5 lg:text-main-title md:text-7xl text-6xl text-stone-300 rounded-md bg-primary capitalize font-title opacity-90"
+        class="flex items-center justify-center w-full h-96 mb-0 bg-fixed bg-center bg-cover lg:p-32 p-10 bg-main-page2"
+        // style="background-image: url(./imgs/second_main.jpg)"
       >
-        CONTUSO
+        <p class="p-5 lg:text-2xl text-lg text-stone-300 bg-primary bg-opacity-80 rounded-xl font-title text-center shadow-2xl">
+          "Contuso provides the most reliable car parts we have ever seen. We
+          recommend Contuso to anyone who wants the best quiality"
+          <br />
+          <b>
+            -<i>Top Gear Magazine</i>
+          </b>
+        </p>
       </div>
-    </section>
-    <div class="w-auto bg-primary p-10">
-      <div class="max-w-lg m-auto">
-        <h1
-          class="text-5xl text-center ml-0 font-title uppercase mb-4 text-stone-300"
-        >
-          The Most Reliable Car parts in the industry
+      <section
+        id="products"
+        class="flex flex-col items-center justify-center bg-primary"
+      >
+        <h1 className="text-5xl font-bold font-title p-8 text-stone-300 uppercase">
+          Our Products
         </h1>
-        <p class="mb-4 m-auto text-lg text-left font-content text-secondary">
-          From all over the globe our products have impacted thousands of lifes
-          by imporing the way their cars function. Our teams focus on
-          maximizing the quality of our car parts so that you never have to worry about a car malfunction
-        </p>
+        <br></br>
+        <ul className="flex lg:flex-row justify-between flex-col items-center">
+          {products
+            ? products.map((product) => (
+                <>
+                  <li key={product.id}>
+                    <ItemCard product={product}></ItemCard>
+                  </li>
+                  <br></br>
+                </>
+              ))
+            : null}
+        </ul>
+      </section>
+      <footer class="text-stone-300 bg-primary">
+        <div class="max-w-3xl mx-auto py-6">
+          <hr class="h-px my-6 bg-primary" />
+          <div class="flex flex-col items-center justify-between mt-6 md:flex-row">
+            <div>
+              <a
+                href="#"
+                class="text-xl font-bold font-title text-stone-300 hover:text-stone-400"
+              >
+                CONTUSO&copy;
+              </a>
+            </div>
 
-        <p class="mb-4 m-auto text-lg text-left font-content text-secondary">
-        We provide personal vehicle owners and enthusiasts with the <b>vehicle related products</b> and <b>knowledge</b> that fulfill their wants and needs at the right price.
-        </p>
-      </div>
-    </div>
-    <div
-      class="flex items-center justify-center w-full h-96 mb-0 bg-fixed bg-center bg-cover lg:p-32 p-10 bg-main-page2"
-      // style="background-image: url(./imgs/second_main.jpg)"
-    >
-      <p
-        class="p-5 lg:text-2xl text-lg text-stone-300 bg-primary bg-opacity-80 rounded-xl font-title text-center shadow-2xl"
-      >
-        "Contuso provides the most reliable car parts we have ever seen. We recommend Contuso to anyone who wants the best quiality"<br />
-        <b>-<i>Top Gear Magazine</i></b>
-      </p>
-    </div>
-    <section id="products" class="flex flex-col items-center justify-center bg-primary">      
-      <h1 className="text-5xl font-bold font-title p-8 text-stone-300 uppercase">Our Products</h1>
-      <br></br>
-      <ul className="flex lg:flex-row justify-between flex-col items-center">
-        {products
-          ? products.map((product) => (
-              <>
-                <li key={product.id}>
-                  <ItemCard product={product}></ItemCard>
-                </li>
-                <br></br>
-              </>
-            ))
-          : null}
-      </ul>
-    </section>
-    <footer class="text-stone-300 bg-primary">
-      <div class="max-w-3xl mx-auto py-6">
-        <hr class="h-px my-6 bg-primary" />
-        <div
-          class="flex flex-col items-center justify-between mt-6 md:flex-row"
-        >
-          <div>
-            <a
-              href="#"
-              class="text-xl font-bold font-title text-stone-300 hover:text-stone-400"
-              >CONTUSO&copy;</a
-            >
-          </div>
-
-          <div class="flex mt-4 md:m-0">
-            <div class="-m-x4">
-              <a
-                href="https://www.instagram.com/carpartscom/?hl=en"
-                target="_blank"
-                class="px-4 text-sm text-stone-300 font-content hover:text-stone-400"
-                >Instagram</a
-              >
-              <a
-                href="https://www.facebook.com/carpartscom/"
-                target="_blank"
-                class="px-4 text-sm text-stone-300 font-content hover:text-stone-400"
-                >Facebook</a
-              >
-              <a
-                href="https://twitter.com/search?lang=en&q=%23carparts"
-                target="_blank"
-                class="px-4 text-sm text-stone-300 font-content hover:text-stone-400"
-                >Twitter</a
-              >
+            <div class="flex mt-4 md:m-0">
+              <div class="-m-x4">
+                <a
+                  href="https://www.instagram.com/carpartscom/?hl=en"
+                  target="_blank"
+                  class="px-4 text-sm text-stone-300 font-content hover:text-stone-400"
+                >
+                  Instagram
+                </a>
+                <a
+                  href="https://www.facebook.com/carpartscom/"
+                  target="_blank"
+                  class="px-4 text-sm text-stone-300 font-content hover:text-stone-400"
+                >
+                  Facebook
+                </a>
+                <a
+                  href="https://twitter.com/search?lang=en&q=%23carparts"
+                  target="_blank"
+                  class="px-4 text-sm text-stone-300 font-content hover:text-stone-400"
+                >
+                  Twitter
+                </a>
+              </div>
             </div>
           </div>
         </div>
-      </div>
-    </footer>
+      </footer>
     </>
   );
 }
